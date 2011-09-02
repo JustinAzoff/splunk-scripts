e9ca7fbce44af45d8417d32004e443d8636f32e9
Justin Azoff <justin@bouncybouncy.net>
2011-09-02 14:55:31 -0400

    try to fix mobile safari detection again..
    new versions of safari on the ipad don't actually say 'mobile' anymore

--- ua2os.py
+++ ua2os.py
@@ -35,6 +35,7 @@ browser_mapping = (
     ('droid',               'Android'),
     ('Chrome',              'Chrome'),
     ('Mobile.*Safari',      'Safari - mobile'),
+    ('i(pod|pad|phone).*Safari', 'Safari - mobile'),
     ('Safari/',             'Safari'),
     ('iTunes',              'iTunes'),
     ('Firefox/(\d)',        'Firefox %s'),
